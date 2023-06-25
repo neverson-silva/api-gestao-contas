62732b11eb70e8b71e9592ae0cf3f3716f7a27cd
Neverson Bento da Silva <neversonbs13@gmail.com>
2023-06-25 13:37:33 -0300

    ajuste tipagem

--- src/app/pessoa/pessoa.provider.ts
+++ src/app/pessoa/pessoa.provider.ts
@@ -2,7 +2,6 @@ import { MesRepository } from '@app/conta/repositories/mes.repository';
 import { ResumoFaturaRepository } from '@app/conta/repositories/resumo-fatura.repository';
 import { Provider } from '@nestjs/common';
 import { PessoaRepository } from './repositories/pessoa.repository';
-import { IPessoaService } from '@app/pessoa/interfaces/pessoa.service.interface';
 import { PessoaService } from '@app/pessoa/services/pessoa.service';
 
 export const pessoaProvider: Provider[] = [

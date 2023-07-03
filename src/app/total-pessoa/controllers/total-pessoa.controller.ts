30d356c45d04dec9e3201ebcb48165ec3168e266
Neverson Bento da Silva <neversonbs13@gmail.com>
2023-07-03 00:04:34 -0300

    endpoint novo atualizacao total pessoa mes

--- src/app/total-pessoa/controllers/total-pessoa.controller.ts
+++ src/app/total-pessoa/controllers/total-pessoa.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Inject, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { ITotalPessoaService } from '../interfaces/total-pessoa.service.interface';
 import { MesAnoDTO } from '@app/conta/dtos/mes-ano.dto';
 import { TotalPessoa } from '../models/total-pessoa.entity';
@@ -13,5 +21,13 @@ export class TotalPessoaController {
   @Get()
   async buscarTotais(@Query() mesAnoDto: MesAnoDTO): Promise<TotalPessoa[]> {
     return await this.totalPessoaService.buscarTotaisPorMesAno(mesAnoDto);
+  }
+
+  @Put(':id')
+  async atualizarTotalPessoa(
+    @Param('id') id: number,
+    @Body('valorPago') valorPago: number,
+  ): Promise<TotalPessoa> {
+    return await this.totalPessoaService.atualizarValorPago(id, { valorPago });
   }
 }
--- src/app/total-pessoa/interfaces/total-pessoa.service.interface.ts
+++ src/app/total-pessoa/interfaces/total-pessoa.service.interface.ts
@@ -1,10 +1,15 @@
 import { ManagebleRepository } from '@infra/interfaces/repository';
 import { TotalPessoa } from '@app/total-pessoa/models/total-pessoa.entity';
 import { MesAnoDTO } from '@app/conta/dtos/mes-ano.dto';
+import { DeepPartial } from 'typeorm';
 
 export interface ITotalPessoaService extends ManagebleRepository {
-  buscarTotaisPorMesAno(
-    mesAnoDto: MesAnoDTO,
-  ): TotalPessoa[] | Promise<TotalPessoa[]>;
+  buscarTotaisPorMesAno(mesAnoDto: MesAnoDTO): Promise<TotalPessoa[]>;
+
   salvarPessoas(mesAno: MesAnoDTO): Promise<TotalPessoa[]>;
+
+  atualizarValorPago(
+    id: number,
+    totalPessoa: DeepPartial<TotalPessoa>,
+  ): Promise<TotalPessoa>;
 }
